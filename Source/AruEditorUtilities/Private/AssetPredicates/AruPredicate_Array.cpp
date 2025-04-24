b6a44ebc4fe6b585f13fa4f59eb7e93a5c0869f2
ZarzaNg <45487646+ZarzaNg@users.noreply.github.com>
2025-04-24 10:14:17 +0800

    feat: add array operation.

---
+++ Source/AruEditorUtilities/Private/AssetPredicates/AruPredicate_Array.cpp
@@ -0,0 +1,130 @@
+﻿#include "AssetPredicates/AruPredicate_Array.h"
+
+void FAruPredicate_AddArrayValue::Execute(FProperty* InProperty, void* InContainer, void* InValue) const
+{
+	const FArrayProperty* ArrayProperty = CastField<FArrayProperty>(InProperty);
+	if(ArrayProperty == nullptr)
+	{
+		return;
+	}
+
+	if(!Predicate.IsValid())
+	{
+		return;
+	}
+
+	const FAruPredicate* PredicatePtr = Predicate.GetPtr<FAruPredicate>();
+	if(PredicatePtr == nullptr)
+	{
+		return;
+	}
+	
+	FScriptArrayHelper ArrayHelper(ArrayProperty, InValue);
+	int32 NewElementIndex = ArrayHelper.AddUninitializedValue();
+	if(!ArrayHelper.IsValidIndex(NewElementIndex))
+	{
+		return;
+	}
+	
+	FProperty* ElementProperty = ArrayProperty->Inner;
+	void* NewElementPtr = ArrayHelper.GetRawPtr(NewElementIndex);
+	PredicatePtr->Execute(ElementProperty, InContainer, NewElementPtr);
+}
+
+void FAruPredicate_RemoveArrayValue::Execute(FProperty* InProperty, void* InContainer, void* InValue) const
+{
+	const FArrayProperty* ArrayProperty = CastField<FArrayProperty>(InProperty);
+	if(ArrayProperty == nullptr)
+	{
+		return;
+	}
+
+	if(Filters.Num() == 0)
+	{
+		return;
+	}
+
+	auto ShouldRemove = [&](void* ValuePtr)
+	{
+		for(const TInstancedStruct<FAruFilter>& FilterStruct : Filters)
+		{
+			const FAruFilter* Filter = FilterStruct.GetPtr<FAruFilter>();
+			if(Filter == nullptr)
+			{
+				continue;
+			}
+
+			if(!Filter->IsConditionMet(ArrayProperty->Inner, InContainer, ValuePtr))
+			{
+				return false;
+			}
+		}
+		return true;
+	};
+
+	TArray<int32> PendingRemove;
+	FScriptArrayHelper ArrayHelper(ArrayProperty, InValue);
+	for(int32 Index = 0; Index < ArrayHelper.Num(); ++Index)  
+	{
+		if(ShouldRemove(ArrayHelper.GetRawPtr(Index)))
+		{
+			PendingRemove.Add(Index);
+		}
+	}
+
+	for(int32& Index : PendingRemove)
+	{
+		ArrayHelper.RemoveValues(Index);
+	}
+}
+
+void FAruPredicate_ModifyArrayValue::Execute(FProperty* InProperty, void* InContainer, void* InValue) const
+{
+	const FArrayProperty* ArrayProperty = CastField<FArrayProperty>(InProperty);
+	if(ArrayProperty == nullptr)
+	{
+		return;
+	}
+
+	if(Predicates.Num() == 0)
+	{
+		return;
+	}
+
+	auto ShouldExecute = [&](void* ValuePtr)
+	{
+		for(const TInstancedStruct<FAruFilter>& FilterStruct : Filters)
+		{
+			const FAruFilter* Filter = FilterStruct.GetPtr<FAruFilter>();
+			if(Filter == nullptr)
+			{
+				continue;
+			}
+
+			if(!Filter->IsConditionMet(ArrayProperty->Inner, InContainer, ValuePtr))
+			{
+				return false;
+			}
+		}
+		return true;
+	};
+
+	FScriptArrayHelper ArrayHelper(ArrayProperty, InValue);
+	for(int32 Index = 0; Index < ArrayHelper.Num(); ++Index)  
+	{
+		void* ElementPtr = ArrayHelper.GetRawPtr(Index);
+		if(ShouldExecute(ElementPtr))
+		{
+			for(const TInstancedStruct<FAruPredicate>& PredicateStruct : Predicates)
+			{
+				const FAruPredicate* Predicate = PredicateStruct.GetPtr<FAruPredicate>();
+				if(Predicate == nullptr)
+				{
+					continue;
+				}
+
+				Predicate->Execute(ArrayProperty->Inner, InContainer, ElementPtr);
+			}
+		}
+	}
+}
---
+++ Source/AruEditorUtilities/Public/AssetPredicates/AruPredicate_Array.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "AruTypes.h"
+#include "AruPredicate_Array.generated.h"
+
+USTRUCT(BlueprintType, DisplayName="Add Value to Array")
+struct FAruPredicate_AddArrayValue : public FAruPredicate
+{
+	GENERATED_BODY()
+public:
+	virtual ~FAruPredicate_AddArrayValue() override {};
+	virtual void Execute(FProperty* InProperty, void* InContainer, void* InValue) const override;
+protected:
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TInstancedStruct<FAruPredicate> Predicate;
+};
+
+
+USTRUCT(BlueprintType, DisplayName="Remove Value(s) from Array")
+struct FAruPredicate_RemoveArrayValue : public FAruPredicate
+{
+	GENERATED_BODY()
+public:
+	virtual ~FAruPredicate_RemoveArrayValue() override {};
+	virtual void Execute(FProperty* InProperty, void* InContainer, void* InValue) const override;
+protected:
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruFilter>> Filters;
+};
+
+USTRUCT(BlueprintType, DisplayName="Modify Value(s) from Array")
+struct FAruPredicate_ModifyArrayValue : public FAruPredicate
+{
+	GENERATED_BODY()
+public:
+	virtual ~FAruPredicate_ModifyArrayValue() override{};
+	virtual void Execute(FProperty* InProperty, void* InContainer, void* InValue) const override;
+protected:
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruFilter>> Filters;
+	
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruPredicate>> Predicates;
+};
