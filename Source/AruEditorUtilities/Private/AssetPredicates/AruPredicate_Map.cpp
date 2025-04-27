eb747739ee0f9e660e7329ef00b06eacfb40a281
ZarzaNg <45487646+ZarzaNg@users.noreply.github.com>
2025-04-27 10:42:58 +0800

    feat: support map operation.

---
+++ Source/AruEditorUtilities/Private/AssetPredicates/AruPredicate_Map.cpp
@@ -0,0 +1,210 @@
+﻿#include "AssetPredicates/AruPredicate_Map.h"
+
+void FAruPredicate_AddMapPair::Execute(FProperty* InProperty, void* InContainer, void* InValue) const
+{
+	const FMapProperty* MapProperty = CastField<FMapProperty>(InProperty);
+	if(MapProperty == nullptr)
+	{
+		return;
+	}
+
+	FScriptMapHelper MapHelper{MapProperty, InValue};
+	int32 NewElementIndex = MapHelper.AddDefaultValue_Invalid_NeedsRehash();
+	if(!MapHelper.IsValidIndex(NewElementIndex))
+	{
+		return;
+	}
+
+	ON_SCOPE_EXIT
+	{
+		MapHelper.Rehash();
+	};
+
+	FProperty* KeyProperty = MapProperty->KeyProp;
+	void* KeyPtr = MapHelper.GetKeyPtr(NewElementIndex);
+	if(KeyProperty == nullptr || KeyPtr == nullptr)
+	{
+		return;
+	}
+	
+	FProperty* ValueProperty = MapProperty->ValueProp;
+	void* ValuePtr = MapHelper.GetValuePtr(NewElementIndex);
+	if(ValueProperty == nullptr || ValuePtr == nullptr)
+	{
+		return;
+	}
+
+	for(auto& Predicate : PredicatesForKey)
+	{
+		if(const FAruPredicate* PredicatePtr = Predicate.GetPtr<FAruPredicate>())
+		{
+			PredicatePtr->Execute(KeyProperty, InContainer, KeyPtr);
+		}
+	}
+
+	for(auto& Predicate : PredicatesForValue)
+	{
+		if(const FAruPredicate* PredicatePtr = Predicate.GetPtr<FAruPredicate>())
+		{
+			PredicatePtr->Execute(ValueProperty, InContainer, ValuePtr);
+		}
+	}
+}
+
+void FAruPredicate_RemoveMapPair::Execute(FProperty* InProperty, void* InContainer, void* InValue) const
+{
+	if(KeyFilters.Num() == 0 && ValueFilters.Num() == 0)
+	{
+		return;
+	}
+	
+	const FMapProperty* MapProperty = CastField<FMapProperty>(InProperty);
+	if(MapProperty == nullptr)
+	{
+		return;
+	}
+
+	auto ShouldRemove = [&](void* KeyPtr, void* ValuePtr)
+	{
+		for(const TInstancedStruct<FAruFilter>& FilterStruct : KeyFilters)
+		{
+			const FAruFilter* Filter = FilterStruct.GetPtr<FAruFilter>();
+			if(Filter == nullptr)
+			{
+				continue;
+			}
+
+			if(!Filter->IsConditionMet(MapProperty->KeyProp, InContainer, KeyPtr))
+			{
+				return false;
+			}
+		}
+
+		for(const TInstancedStruct<FAruFilter>& FilterStruct : ValueFilters)
+		{
+			const FAruFilter* Filter = FilterStruct.GetPtr<FAruFilter>();
+			if(Filter == nullptr)
+			{
+				continue;
+			}
+
+			if(!Filter->IsConditionMet(MapProperty->ValueProp, InContainer, ValuePtr))
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	};
+
+	TArray<int32> PendingRemove;
+	FScriptMapHelper MapHelper{MapProperty, InValue};
+	
+	ON_SCOPE_EXIT
+	{
+		MapHelper.Rehash();
+	};
+	
+	for(int32 Index = 0; Index < MapHelper.Num(); ++Index)  
+	{         
+		void* MapKeyPtr = MapHelper.GetKeyPtr(Index);  
+		void* MapValuePtr = MapHelper.GetValuePtr(Index);
+
+		if(ShouldRemove(MapKeyPtr, MapValuePtr))
+		{
+			PendingRemove.Add(Index);
+		}
+	}
+
+	for(int32& Index : PendingRemove)
+	{
+		MapHelper.RemoveAt(Index);
+	}
+}
+
+void FAruPredicate_ModifyMapPair::Execute(FProperty* InProperty, void* InContainer, void* InValue) const
+{
+	if(KeyFilters.Num() == 0 && ValueFilters.Num() == 0)
+	{
+		return;
+	}
+	
+	const FMapProperty* MapProperty = CastField<FMapProperty>(InProperty);
+	if(MapProperty == nullptr || MapProperty->KeyProp == nullptr || MapProperty->ValueProp == nullptr)
+	{
+		return;
+	}
+
+	auto ShouldModify = [&](void* KeyPtr, void* ValuePtr)
+	{
+		for(const TInstancedStruct<FAruFilter>& FilterStruct : KeyFilters)
+		{
+			const FAruFilter* Filter = FilterStruct.GetPtr<FAruFilter>();
+			if(Filter == nullptr)
+			{
+				continue;
+			}
+
+			if(!Filter->IsConditionMet(MapProperty->KeyProp, InContainer, KeyPtr))
+			{
+				return false;
+			}
+		}
+
+		for(const TInstancedStruct<FAruFilter>& FilterStruct : ValueFilters)
+		{
+			const FAruFilter* Filter = FilterStruct.GetPtr<FAruFilter>();
+			if(Filter == nullptr)
+			{
+				continue;
+			}
+
+			if(!Filter->IsConditionMet(MapProperty->ValueProp, InContainer, ValuePtr))
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	};
+
+	TArray<int32> PendingToModify;
+	FScriptMapHelper MapHelper{MapProperty, InValue};
+	
+	ON_SCOPE_EXIT
+	{
+		MapHelper.Rehash();
+	};
+	
+	for(int32 Index = 0; Index < MapHelper.Num(); ++Index)
+	{
+		void* MapKeyPtr = MapHelper.GetKeyPtr(Index);  
+		void* MapValuePtr = MapHelper.GetValuePtr(Index);
+
+		if(ShouldModify(MapKeyPtr, MapValuePtr))
+		{
+			PendingToModify.Add(Index);
+		}
+	}
+
+	for(int32& Index : PendingToModify)
+	{
+		void* MapKeyPtr = MapHelper.GetKeyPtr(Index);  
+		for(auto& Predicate : PredicatesForKey)
+		{
+			if(const FAruPredicate* PredicatePtr = Predicate.GetPtr<FAruPredicate>())
+			{
+				PredicatePtr->Execute(MapProperty->KeyProp, InContainer, MapKeyPtr);
+			}
+		}
+
+		void* MapValuePtr = MapHelper.GetValuePtr(Index);
+		for(auto& Predicate : PredicatesForValue)
+		{
+			if(const FAruPredicate* PredicatePtr = Predicate.GetPtr<FAruPredicate>())
+			{
+				PredicatePtr->Execute(MapProperty->ValueProp, InContainer, MapValuePtr);
+			}
+		}
+	}
+}
---
+++ Source/AruEditorUtilities/Public/AssetPredicates/AruPredicate_Map.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "AruTypes.h"
+#include "AruPredicate_Map.generated.h"
+
+USTRUCT(BlueprintType, DisplayName="Add Pair to Map")
+struct FAruPredicate_AddMapPair : public FAruPredicate
+{
+	GENERATED_BODY()
+public:
+	virtual ~FAruPredicate_AddMapPair() override {};
+	virtual void Execute(FProperty* InProperty, void* InContainer, void* InValue) const override;
+protected:
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruPredicate>> PredicatesForKey;
+
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruPredicate>> PredicatesForValue;
+};
+
+USTRUCT(BlueprintType, DisplayName="Remove Pair(s) from Map")
+struct FAruPredicate_RemoveMapPair : public FAruPredicate
+{
+	GENERATED_BODY()
+public:
+	virtual ~FAruPredicate_RemoveMapPair() override {};
+	virtual void Execute(FProperty* InProperty, void* InContainer, void* InValue) const override;
+protected:
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruFilter>> KeyFilters;
+
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruFilter>> ValueFilters;
+};
+
+USTRUCT(BlueprintType, DisplayName="Modify Pair(s) from Map")
+struct FAruPredicate_ModifyMapPair : public FAruPredicate
+{
+	GENERATED_BODY()
+public:
+	virtual ~FAruPredicate_ModifyMapPair() override {};
+	virtual void Execute(FProperty* InProperty, void* InContainer, void* InValue) const override;
+protected:
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruFilter>> KeyFilters;
+
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruFilter>> ValueFilters;
+
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruPredicate>> PredicatesForKey;
+
+	UPROPERTY(EditDefaultsOnly, meta=(ExcludeBaseStruct))
+	TArray<TInstancedStruct<FAruPredicate>> PredicatesForValue;
+};
+
+
