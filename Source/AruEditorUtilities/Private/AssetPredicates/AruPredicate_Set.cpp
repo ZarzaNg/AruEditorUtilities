1176e6c0f482b759382be08a36b65e3302aa91bc
ZarzaNg <45487646+ZarzaNg@users.noreply.github.com>
2025-04-28 23:49:27 +0800

    feat: Add set operation implementation.

--- Source/AruEditorUtilities/Private/AssetPredicates/AruPredicate_Set.cpp
+++ Source/AruEditorUtilities/Private/AssetPredicates/AruPredicate_Set.cpp
@@ -42,10 +42,98 @@ void FAruPredicate_AddSetElement::Execute(FProperty* InProperty, void* InContain
 
 void FAruPredicate_RemoveSetValue::Execute(FProperty* InProperty, void* InContainer, void* InValue) const
 {
-	FAruPredicate::Execute(InProperty, InContainer, InValue);
+	const FSetProperty* SetProperty = CastField<FSetProperty>(InProperty);
+	if(SetProperty == nullptr)
+	{
+		return;
+	}
+
+	if(Filters.Num() == 0)
+	{
+		return;
+	}
+	
+	auto ShouldRemove = [&](void* ValuePtr)
+	{
+		for(const TInstancedStruct<FAruFilter>& FilterStruct : Filters)
+		{
+			const FAruFilter* Filter = FilterStruct.GetPtr<FAruFilter>();
+			if(Filter == nullptr)
+			{
+				continue;
+			}
+
+			if(!Filter->IsConditionMet(SetProperty->ElementProp, InContainer, ValuePtr))
+			{
+				return false;
+			}
+		}
+		return true;
+	};
+
+	TArray<int32> PendingRemove;
+	FScriptSetHelper SetHelper{SetProperty, InValue};
+	for(int32 Index = 0; Index < SetHelper.Num(); ++Index)  
+	{
+		if(ShouldRemove(SetHelper.GetElementPtr(Index)))
+		{
+			PendingRemove.Add(Index);
+		}
+	}
+
+	for(int32& Index : PendingRemove)
+	{
+		SetHelper.RemoveAt(Index);
+	}
 }
 
 void FAruPredicate_ModifySetValue::Execute(FProperty* InProperty, void* InContainer, void* InValue) const
 {
-	FAruPredicate::Execute(InProperty, InContainer, InValue);
+	const FSetProperty* SetProperty = CastField<FSetProperty>(InProperty);
+	if(SetProperty == nullptr)
+	{
+		return;
+	}
+
+	if(Filters.Num() == 0)
+	{
+		return;
+	}
+	
+	auto ShouldModify = [&](void* ValuePtr)
+	{
+		for(const TInstancedStruct<FAruFilter>& FilterStruct : Filters)
+		{
+			const FAruFilter* Filter = FilterStruct.GetPtr<FAruFilter>();
+			if(Filter == nullptr)
+			{
+				continue;
+			}
+
+			if(!Filter->IsConditionMet(SetProperty->ElementProp, InContainer, ValuePtr))
+			{
+				return false;
+			}
+		}
+		return true;
+	};
+
+	FScriptSetHelper SetHelper{SetProperty, InValue};
+	for(int32 Index = 0; Index < SetHelper.Num(); ++Index)  
+	{
+		void* ElementPtr = SetHelper.GetElementPtr(Index);
+		if(ShouldModify(ElementPtr))
+		{
+			for(const TInstancedStruct<FAruPredicate>& PredicateStruct : Predicates)
+			{
+				const FAruPredicate* Predicate = PredicateStruct.GetPtr<FAruPredicate>();
+				if(Predicate == nullptr)
+				{
+					continue;
+				}
+
+				Predicate->Execute(SetProperty->ElementProp, InContainer, ElementPtr);
+			}
+		}
+	}
 }
