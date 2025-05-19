991123b05040e3713465a22f3adc6c8d3aabf504
ZarzaNg <45487646+ZarzaNg@users.noreply.github.com>
2025-05-20 00:39:49 +0800

    feat: Add path redirector.

---
+++ Source/AruEditorUtilities/Private/AssetPredicates/AruPredicate_AssetPathRedirector.cpp
@@ -0,0 +1,54 @@
+﻿#include "AssetPredicates/AruPredicate_AssetPathRedirector.h"
+
+void FAruPredicate_AssetPathRedirector::Execute(const FProperty* InProperty, void* InValue, const FInstancedPropertyBag& InParameters) const
+{
+	if(InProperty == nullptr || InValue == nullptr)
+	{
+		return;
+	}
+	
+	const FObjectProperty* ObjectProperty = CastField<FObjectProperty>(InProperty);
+	if(ObjectProperty == nullptr)
+	{
+		return;
+	}
+
+	UObject* ObjectPtr = ObjectProperty->GetObjectPropertyValue(InValue);
+	if(ObjectPtr == nullptr)
+	{
+		return;
+	}
+	
+	FString OriginalPath = ObjectPtr->GetPathName();
+
+	TArray<FString> PathSegments;
+	OriginalPath.ParseIntoArray(PathSegments, TEXT("/"), true);
+
+	TArray<FString> SortedKeys;
+	ReplacementMap.GetKeys(SortedKeys);
+	SortedKeys.Sort([](const FString& A, const FString& B) {
+		return A.Len() > B.Len(); 
+	});
+	
+	for (FString& Segment : PathSegments)
+	{
+		FString ModifiedSegment = Segment;
+		for (const FString& Key : SortedKeys)
+		{
+			if (ModifiedSegment.Contains(Key))
+			{
+				const FString* Replacement = ReplacementMap.Find(Key);
+				ModifiedSegment = ModifiedSegment.Replace(*Key, **Replacement, ESearchCase::CaseSensitive);
+			}
+		}
+		Segment = ModifiedSegment;
+	}
+
+	FString NewPath = FString::Printf(TEXT("/%s"), *FString::Join(PathSegments, TEXT("/")));
+
+	FSoftObjectPath TargetAssetPath(NewPath);
+	if (UObject* LoadedAsset = TargetAssetPath.TryLoad())
+	{
+		ObjectProperty->SetObjectPropertyValue(InValue, LoadedAsset);
+	}
+}
---
+++ Source/AruEditorUtilities/Public/AssetPredicates/AruPredicate_AssetPathRedirector.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "AruTypes.h"
+#include "AruPredicate_AssetPathRedirector.generated.h"
+
+USTRUCT(BlueprintType, DisplayName="AssetPath Redirector")
+struct FAruPredicate_AssetPathRedirector : public FAruPredicate
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FString, FString> ReplacementMap;
+	
+	virtual ~FAruPredicate_AssetPathRedirector() override {}
+	virtual void Execute(const FProperty* InProperty, void* InValue, const FInstancedPropertyBag& InParameters) const override;
+};
