99aa131fc8985dcf32e4890cb006bfff61599f88
ZarzaNg <45487646+ZarzaNg@users.noreply.github.com>
2025-05-20 10:09:52 +0800

    feat: support different name-matching operations.

--- Source/AruEditorUtilities/Private/AssetFilters/AruFilter_ByName.cpp
+++ Source/AruEditorUtilities/Private/AssetFilters/AruFilter_ByName.cpp
@@ -6,6 +6,42 @@ bool FAruFilter_ByName::IsConditionMet(const FProperty* InProperty, const void* 
 	{
 		return bInverseCondition;
 	}
+
+	if(CompareOp == EAruNameCompareOp::MatchAll)
+	{
+		return (PropertyName == InProperty->GetName() || PropertyName == InProperty->GetDisplayNameText().ToString()) ^ bInverseCondition;
+	}
+	else
+	{
+		return (InProperty->GetName().Contains(PropertyName) || InProperty->GetDisplayNameText().ToString().Contains(PropertyName)) ^ bInverseCondition;
+	}
+}
+
+bool FAruFilter_ByObjectName::IsConditionMet(const FProperty* InProperty, const void* InValue, const FInstancedPropertyBag& InParameters) const
+{
+	if(InProperty == nullptr || InValue == nullptr)
+	{
+		return bInverseCondition;
+	}
 	
-	return (PropertyName == InProperty->GetName() || PropertyName == InProperty->GetDisplayNameText().ToString()) ^ bInverseCondition;
+	const FObjectProperty* ObjectProperty = CastField<FObjectProperty>(InProperty);
+	if(ObjectProperty == nullptr)
+	{
+		return bInverseCondition;
+	}
+
+	UObject* ObjectPtr = ObjectProperty->GetObjectPropertyValue(InValue);
+	if(ObjectPtr == nullptr)
+	{
+		return bInverseCondition;
+	}
+
+	if(CompareOp == EAruNameCompareOp::MatchAll)
+	{
+		return (ObjectName == ObjectPtr->GetName()) ^ bInverseCondition;
+	}
+	else
+	{
+		return (ObjectPtr->GetName().Contains(ObjectName)) ^ bInverseCondition;
+	}
 }
--- Source/AruEditorUtilities/Public/AssetFilters/AruFilter_ByName.h
+++ Source/AruEditorUtilities/Public/AssetFilters/AruFilter_ByName.h
@@ -2,6 +2,14 @@
 #include "AruTypes.h"
 #include "AruFilter_ByName.generated.h"
 
+
+UENUM(BlueprintType)
+enum class EAruNameCompareOp : uint8
+{
+	MatchAny,
+	MatchAll
+};
+
 USTRUCT(BlueprintType, DisplayName="Does Property Name Match")
 struct FAruFilter_ByName : public FAruFilter
 {
@@ -11,5 +19,23 @@ public:
 	virtual bool IsConditionMet(const FProperty* InProperty, const void* InValue, const FInstancedPropertyBag& InParameters) const override;
 protected:
 	UPROPERTY(EditDefaultsOnly, SimpleDisplay)
+	EAruNameCompareOp CompareOp = EAruNameCompareOp::MatchAny;
+	
+	UPROPERTY(EditDefaultsOnly, SimpleDisplay)
 	FString PropertyName{"None"};
+};
+
+USTRUCT(BlueprintType, DisplayName="Does Object Name Match")
+struct FAruFilter_ByObjectName : public FAruFilter
+{
+	GENERATED_BODY()
+public:
+	virtual ~FAruFilter_ByObjectName() override {};
+	virtual bool IsConditionMet(const FProperty* InProperty, const void* InValue, const FInstancedPropertyBag& InParameters) const override;
+protected:
+	UPROPERTY(EditDefaultsOnly, SimpleDisplay)
+	EAruNameCompareOp CompareOp = EAruNameCompareOp::MatchAny;
+	
+	UPROPERTY(EditDefaultsOnly, SimpleDisplay)
+	FString ObjectName{"None"};
 };
